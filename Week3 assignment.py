c28366a13aa12d90da72716afa3b02143c67a3d4
Satpal <satpalkaur@users.noreply.github.com>
2025-03-22 15:12:25 +0530

    Add files via upload

---
+++ Week3 assignment.py
@@ -0,0 +1,31 @@
+# import packages (DO NOT MODIFY)
+import sys
+
+# Your code - START >
+def compress_string(s: str) -> str:
+    # write here...
+    count=1
+    outputstring=""
+    for i in range(1,len(s)):
+      current_char = s[i]
+      previous_char = s[i-1]
+      if current_char == previous_char:
+        count=count+1
+      else:
+        if count>1:
+          outputstring=outputstring+previous_char+str(count)
+        else:
+          outputstring=outputstring+previous_char
+        count=1
+
+    if count>1:
+      outputstring=outputstring+current_char+str(count)
+    else:
+      outputstring=outputstring+current_char
+    return(outputstring)
+# Your code - END >
+
+# main function (DO NOT MODIFY)
+if __name__ == "__main__":
+    s = str(sys.argv[1]).strip()  # Read input from command line
+    print(compress_string(s))  # Print the result
